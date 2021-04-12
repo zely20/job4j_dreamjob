ea3151e2afebfac814b7df414f2015c5f48d5586
Aliaksandr Zelianko <zely20@mail.ru>
2021-04-12 17:07:02 +0300

    8. HTML form. Создание кандидата.[277241#471826]

---
+++ src/main/java/ru/job4j/dreamjob/servlet/PostServlet.java
@@ -0,0 +1,14 @@
+package ru.job4j.dreamjob.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class PostServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
