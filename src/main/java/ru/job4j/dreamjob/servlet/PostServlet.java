e38518d01406e53e0062071fcd6f4b3df7bed8db
zely20@mail.ru <Civik1984>
2021-04-13 21:37:15 +0300

    1. Servlet. Web.xml[6866#471833]

--- src/main/java/ru/job4j/dreamjob/servlet/PostServlet.java
+++ src/main/java/ru/job4j/dreamjob/servlet/PostServlet.java
@@ -1,5 +1,8 @@
 package ru.job4j.dreamjob.servlet;
 
+import ru.job4j.dreamjob.Store;
+import ru.job4j.dreamjob.model.Post;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +12,8 @@ import java.io.IOException;
 public class PostServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        req.setCharacterEncoding("UTF-8");
+        Store.instOf().save(new Post(0, req.getParameter("name")));
+        resp.sendRedirect(req.getContextPath() + "/posts.jsp");
     }
 }
