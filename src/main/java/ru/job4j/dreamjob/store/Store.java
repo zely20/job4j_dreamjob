d008ee5e01956357b14556decadf0349c1c553a5
zely20@mail.ru <Civik1984>
2021-04-14 20:03:51 +0300

    add final

--- src/main/java/ru/job4j/dreamjob/store/Store.java
+++ src/main/java/ru/job4j/dreamjob/store/Store.java
@@ -12,9 +12,8 @@ public class Store {
 
     private static final Store INST = new Store();
     private static AtomicInteger POST_ID = new AtomicInteger(4);
-    private Map<Integer, Post> posts = new ConcurrentHashMap<>();
-
-    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job"));
