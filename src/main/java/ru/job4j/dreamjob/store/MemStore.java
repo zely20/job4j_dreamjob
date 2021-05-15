cf7fcb4f41b4ddb33d6a1813c68bf2ef658c3aad
zely20@mail.ru <Civik1984>
2021-05-15 19:23:11 +0300

    dont work test

--- src/main/java/ru/job4j/dreamjob/store/MemStore.java
+++ src/main/java/ru/job4j/dreamjob/store/MemStore.java
@@ -2,13 +2,14 @@ package ru.job4j.dreamjob.store;
 
 import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.model.Post;
+import ru.job4j.dreamjob.model.User;
 
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class MemStore {
+public class MemStore implements Store {
 
     private static final MemStore INST = new MemStore();
     private static AtomicInteger POST_ID = new AtomicInteger(4);
@@ -16,13 +17,13 @@ public class MemStore {
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
-    private MemStore() {
-        posts.put(1, new Post(1, "Junior Java Job"));
+    public MemStore() {
+        /*posts.put(1, new Post(1, "Junior Java Job"));
         posts.put(2, new Post(2, "Middle Java Job"));
         posts.put(3, new Post(3, "Senior Java Job"));
         candidates.put(1, new Candidate(1, "Junior Java"));
         candidates.put(2, new Candidate(2, "Middle Java"));
-        candidates.put(3, new Candidate(3, "Senior Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));*/
     }
 
     public void save(Post post) {
@@ -45,6 +46,21 @@ public class MemStore {
 
     public Candidate findByIdCandidate(int id) {
         return candidates.get(id);
+    }
+
+    @Override
+    public Integer deleteCandidate(int id) {
+        return null;
+    }
+
+    @Override
+    public void saveUser(User user) {
+
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return null;
     }
 
     public static MemStore instOf() {
--- src/main/test/java/ru.job4j.dreamjob.test/PostServletTest.java
+++ src/main/test/java/ru.job4j.dreamjob.test/PostServletTest.java
@@ -7,6 +7,7 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import ru.job4j.dreamjob.servlet.PostServlet;
+import ru.job4j.dreamjob.store.MemStore;
 import ru.job4j.dreamjob.store.PsqlStore;
 import ru.job4j.dreamjob.store.Store;
 import ru.job4j.dreamjob.stub.ValidateStub;
@@ -22,14 +23,18 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(PowerMockRunner.class)
+//класс который мы поменяем
 @PrepareForTest(PsqlStore.class)
 public class PostServletTest {
 
     @Test
     public void whenAddPostThenStoreIt() throws ServletException, IOException {
-        Store validate = new ValidateStub();
+        //1. Создаем экземпляр класса
+        Store validate = new MemStore();
+        // 2. Это я так понимаю разрешает вызывать статические методы у класса
         PowerMockito.mockStatic(PsqlStore.class);
-        when(PsqlStore.getInstance()).thenReturn(validate);
+        //3. когда создается PsqlStore меняет его на MemStore
+        when(PsqlStore.instOf()).thenReturn(validate);
         HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
         when(req.getParameter("name")).thenReturn("Petr Arsentev");
